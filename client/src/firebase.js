51f3bc3d498027633a636dbf236ba197a6a89118
devansh-mankar <Devanshmankar09@gmail.com>
2024-04-09 00:23:56 +0530

    OAuth

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
+
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "mern-blog-b327f.firebaseapp.com",
-  projectId: "mern-blog-b327f",
-  storageBucket: "mern-blog-b327f.appspot.com",
-  messagingSenderId: "699397991367",
-  appId: "1:699397991367:web:88ff565ef72a182d6b87e2",
+  authDomain: "mern-blog-3ebb5.firebaseapp.com",
+  projectId: "mern-blog-3ebb5",
+  storageBucket: "mern-blog-3ebb5.appspot.com",
+  messagingSenderId: "158238943576",
+  appId: "1:158238943576:web:6c965b7d8d70b9612f9d2d",
+  measurementId: "G-EV5FD0X6PJ",
 };
 
 // Initialize Firebase
