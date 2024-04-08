b77b3946bd1352f9dcb9e50f4a5004f24ee9fec0
devansh-mankar <Devanshmankar09@gmail.com>
2024-04-09 00:30:43 +0530

    dark mode

--- client/src/redux/user/store.js
+++ client/src/redux/user/store.js
@@ -1,6 +1,6 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-import themeReducer from "./theme/themeSlice";
+import userReducer from "./userSlice";
+import themeReducer from "../theme/themeSlice";
 import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
